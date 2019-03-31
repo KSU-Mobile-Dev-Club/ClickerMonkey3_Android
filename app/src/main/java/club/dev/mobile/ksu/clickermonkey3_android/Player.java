4f4b9e55e6751bd5af86a527abe8a3f8c085cf9c
Emma <erj2016@ksu.edu>
2019-03-31 17:13:15 -0500

    Repository Class

---
+++ app/src/main/java/club/dev/mobile/ksu/clickermonkey3_android/Player.java
@@ -0,0 +1,11 @@
+package club.dev.mobile.ksu.clickermonkey3_android;
+
+public class Player {
+    public Player(String s, int i, String k)
+    {
+        String name = s;
+        int score = i;
+        String key = k;
+    }
+
+}
